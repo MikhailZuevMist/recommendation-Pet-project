966e0d9dbb114d6ce739fb77cdd180e9de297b4d
“[Mikhail <mishanet1997@gmail.com>
2023-03-12 15:09:55 +0500

    Change

--- FastAPI/DataBase/Settings.py
+++
@@ -1,6 +0,0 @@
-postgresql = {'pguser': 'postgres',
-              'pgpassword': 'Blade007',
-              'pghost': 'localhost',
-              'pgport': 5432
-              }
-openai = {'password': 'Blade007@'}
